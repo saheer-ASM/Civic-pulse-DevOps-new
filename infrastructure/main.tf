707389e62d5550b71a632400c1b35a937ca5be17
Saheer ASM <mohamedshaheer637@gmail.com>
2026-01-29 21:34:36 +0530

    Infrastructure: Finalize EKS setup with S3 remote state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket = "civic-pulse-tf-state-5304"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
@@ -7,6 +15,7 @@ terraform {
     }
   }
 }
+
 
 provider "aws" {
   region = var.aws_region
